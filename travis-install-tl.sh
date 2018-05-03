1b8a405b23eb470bbd6251f5b8e1513b686afd94
Manas Talukdar <manas.talukdar@gmail.com>
2018-05-03 12:51:12 -0700

    Attempt.

--- travis-install-tl.sh
+++ travis-install-tl.sh
@@ -2,9 +2,7 @@
 if [ -d /usr/local/texlive ]; then
   sudo /opt/texbin/tlmgr update --all
 else
-  cd ../../
   wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu
   chmod +x ./install-tl-ubuntu
   sudo ./install-tl-ubuntu
-  export PATH="/opt/texbin:$PATH"
 fi
