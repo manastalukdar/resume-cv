ec46a53a813767b2e76661c66fa5c308a2c58d3b
Manas Talukdar <manas.talukdar@gmail.com>
2018-05-08 01:14:07 -0700

    added unregister to ServiceWorker

--- website/src/index.js
+++ website/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App url='ManasTalukdar_CV.pdf'/>, document.getElementById('root'));
-//registerServiceWorker();
+unregister()
